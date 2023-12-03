234e6ccaf0af88861de107e8010e50c507f0a95d
Felipe Marinho <github@felipemarinho.dev>
2023-12-03 19:12:05 +0000

    return `updated_at`, `version`, `max_connections` and, `opened_connections` on the `/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as some;')
-  console.log(result.rows);
-  response.status(200).json({ chave: "alunos do curso.dev são pessoas legais!" })
+  const updatedAt = new Date().toISOString();
+
+  const { max_conn, used } = await database.query(`
+  select
+    max_conn,
+    used,
+    res_for_super,
+    max_conn-used-res_for_super res_for_normal 
+  from 
+    (select count(*) used from pg_stat_activity) t1,
+    (
+      select
+        setting::int res_for_super 
+      from 
+        pg_settings 
+      where 
+        name=$$superuser_reserved_connections$$
+    ) t2,
+    (select setting::int max_conn from pg_settings where name=$$max_connections$$) t3
+  `).then(result => result.rows[0])
+
+  const databaseVersion = await database.query('SHOW server_version')
+    .then(result => result.rows[0].server_version)
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: databaseVersion,
+    max_connections: max_conn,
+    opened_connections: used
+  })
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toStrictEqual(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.version).toBeDefined();
+  expect(responseBody.version).not.toBeNaN();
+
+
+  expect(responseBody.opened_connections).toBeDefined();
+  expect(responseBody.opened_connections).not.toBeNaN();
+
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.max_connections).not.toBeNaN();
 });
