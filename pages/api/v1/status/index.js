32edffc4e64b92d610413506c6ee68cef5054e68
Felipe Marinho <github@felipemarinho.dev>
2023-12-24 17:36:09 +0000

    refactor open connections functionality

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,30 +3,14 @@ import database from "infra/database.js"
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const { used } = await database.query(`
-  select
-    max_conn,
-    used,
-    res_for_super,
-    max_conn-used-res_for_super res_for_normal 
-  from 
-    (select count(*) used from pg_stat_activity) t1,
-    (
-      select
-        setting::int res_for_super 
-      from 
-        pg_settings 
-      where 
-        name=$$superuser_reserved_connections$$
-    ) t2,
-    (select setting::int max_conn from pg_settings where name=$$max_connections$$) t3
-  `).then(result => result.rows[0])
-
   const databaseVersionResult = await database.query('SHOW server_version;');
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
   const databaseMaxConnectionsResult = await database.query('SHOW max_connections;');
   const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db' AND state='active';")
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
@@ -34,7 +18,7 @@ async function status(request, response) {
       database: {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
-        opened_connections: used
+        opened_connections: databaseOpenedConnectionsValue
       }
     }
   })
